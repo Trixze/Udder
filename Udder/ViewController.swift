897a4bc8a3c8de5059212ca15a9093eb9ff4af3e
Trixze <tim@timzim.com>
2019-06-30 19:36:52 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tim Nolan on 30/06/2019.
+//  Copyright © 2019 Tim Nolan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
